46f75b1de9c1ec2f3e3d118acf6a10a28a74240d
HimajaReddy <himaja04.selenium@gmail.com>
2021-11-11 07:38:04 +0530

    Assertions unit testing framework

---
+++ LalithaPythonClass/UnitTesting/AssertsinUnitTesing/AssertIn_Method.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class Himaja(unittest.TestCase):
+
+    # test function to test whether key is present in container
+    def test_negative(self):
+        key = "lalitha"
+        container = "Himaja reddy yerrabolu"
+        # error message in case if test case got failed
+        message = "key is not in container."
+        # assertIn() to check if key is in container
+        self.assertIn(key, container, message)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# AssertionError: 'lalitha' not found in 'Himaja reddy yerrabolu' : key is not in container.
